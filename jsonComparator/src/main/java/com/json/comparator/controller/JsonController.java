c69f2bf0d1a021c6c82e1b7f31d4d8b40a14a2af
firozwkhan92 <firozwkhan92@gmail.com>
2020-04-25 03:18:42 +0530

    upadating

--- jsonComparator/src/main/java/com/json/comparator/controller/JsonController.java
+++ jsonComparator/src/main/java/com/json/comparator/controller/JsonController.java
@@ -1,18 +1,13 @@
 package com.json.comparator.controller;
 
-import java.io.IOException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.json.comparator.exceptions.JsonComparatorExceptions;
 import com.json.comparator.model.InputData;
 import com.json.comparator.service.JsonCompareService;
